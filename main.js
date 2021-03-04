64fb5cff70a596c798daf90a5cd324417fa762ed
songyi <nice.dev.song@gmail.com>
2021-03-04 16:11:03 +0900

    update js-smooth scroll event

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ function navbar(){
     nav.style.transform = 'translateY(-60px)';
     }}
  window.addEventListener('scroll',navbar)
+
+// 2. navigation menu smooth scroll
+const menu = document.querySelector('.menu');
+const basicMenu = document.querySelector('.basic-menu');
+function smoothScroll(e){
+    if(e.target.dataset.key === 'home'){
+        document.documentElement.scrollTop = 0;
+    };
+}
+menu.addEventListener('click',smoothScroll);
+basicMenu.addEventListener('click',smoothScroll);
